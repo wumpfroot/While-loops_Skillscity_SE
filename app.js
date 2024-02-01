21de0683f61270330905293aeebd5d3da50c560c
wumpfroot <markuskojo@msn.com>
2024-02-01 15:23:58 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//while-loops - Task 10
+
+//loop that countdowns to zero
+console.log("While loop");
+let i = 10;
+
+while (i >= 0) {
+	console.log(i);
+	i--;
+}
+
+//do while loop
+console.log("Same countdown, but using a DO WHILE loop");
+let index = 10;
+do {
+	console.log(index);
+	index--;
+} while (index >= 0);
